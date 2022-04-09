bfa9483cab6d0083a4d7d9647fd532e42b3e05f3
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-09 11:36:38 +0300

    create faretripler function

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo']
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
 function returnFirstTwoDrivers (){
-    return drivers.slice(0 , 2)
+    return drivers.slice(0 , 2);
 }
 function returnLastTwoDrivers(){
-    return drivers.slice(2)
+    return drivers.slice(2);
 }
-const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
 function createFareMultiplier(){
     return (fare => fare * fare);
 }
- const fareDoubler =(fare)=>{
-     return fare * 2
- }
+const fareDoubler =(fare)=>{
+     return fare * 2;
+}
+const fareTripler = (fare) => {
+    return fare * 3;
+}
